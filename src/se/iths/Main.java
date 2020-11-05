eb53a85ede3b63279d8b4bc0be6af69d63c06d4a
Pontus REDIG <pontus.redig@alten.se>
2020-11-05 10:46:33 +0100

    Second commit

--- src/se/iths/Main.java
+++ src/se/iths/Main.java
@@ -3,6 +3,6 @@ package se.iths;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("First version");
+        System.out.println("Second version");
     }
 }
